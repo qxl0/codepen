f07d44df3a8689d6045593c8dfcc637a01f06a61
Qiang <qxl0@yahoo.com>
2021-12-10 09:46:30 -0500

    localstorage, fontawesome

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 import 'codemirror/lib/codemirror.css'
 import 'codemirror/theme/material.css'
 import 'codemirror/mode/xml/xml'
 import 'codemirror/mode/javascript/javascript'
 import 'codemirror/mode/css/css'
 import { Controlled as ControlledEditor }  from 'react-codemirror2'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCompressAlt, faExpandAlt } from '@fortawesome/free-solid-svg-icons'
+
 
 function Editor({ displayName, language, value, onChange }) {
   const handleChange = (editor, data, value) => {
     onChange(value)
   }
+  const [open, setOpen] = useState(true);
+
   return (
-    <div className="editor-container">
+    <div className={`editor-container ${open? '' : 'collapsed'}`}>
       <div className="editor-title">
         {displayName}
-        <button>O/C</button>
+        <button 
+          type="button"
+          className="expand-collapse-btn"
+          onClick={()=>setOpen(prevOpen => !prevOpen)}>
+          <FontAwesomeIcon icon={open? faExpandAlt: faCompressAlt}></FontAwesomeIcon>
+        </button>
       </div>
       <ControlledEditor onBeforeChange={handleChange}
         value={value}
